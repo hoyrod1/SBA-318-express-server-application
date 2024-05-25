288b9e75a1ced446fd114054b27682f106ed4695
Rodney St Cloud <hoyrod1@aol.com>
2024-05-25 00:06:25 -0400

    Connected the index.js file in the routes directory to the index.ejs template landing page and rendered a view. Also the JSON middleware handler, The set up for the express urlencoded niddleware handler has been created and the express.static has been set up to first retrieve static data from the public directory

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+const { response } = require("express");
 const express = require("express");
+const { request } = require("http");
 const indexRouter = require("./routes/index.js");
 const app = express();
 const port = 8080;
 
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
+
+// app.use((error, request, response, next) => {
+//   response.status(500).send(`There was a server error`);
+// });
+
 app.listen(port, () => {
-  console.log(`This server is listening on prt: ${port}`);
+  console.log(`This server is listening on port: ${port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { url: "./views/home.ejs" });
+  res.render("index", {
+    title: "SBA Express Server Application",
+    url: "./views/home.ejs",
+  });
 });
 
 module.exports = router;
