615b747ee5d9656d26f909fb425286c25d536f04
Rodney St Cloud <hoyrod1@aol.com>
2024-05-24 23:11:52 -0400

    Created a routes directory with a index.js file to set up a get route to the index.ejs file landing page located in the views directory

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const express = require("express");
 const indexRouter = require("./routes/index.js");
 const app = express();
 const port = 8080;
+
+app.listen(port, () => {
+  console.log(`This server is listening on prt: ${port}`);
+});
